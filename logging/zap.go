0ec5b3184cc9e7b4805f3f4a837d327ec76e7031
unknown <zouhy>
2020-10-09 08:49:35 +0800

    add NewProdLoggger function

--- logging/zap.go
+++ logging/zap.go
@@ -2,9 +2,11 @@ package logging
 
 import (
 	"fmt"
+	"os"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func NewSimpleLogger() *zap.Logger {
@@ -38,5 +40,67 @@ func NewSimpleLogger() *zap.Logger {
 	if err != nil {
 		panic(fmt.Sprintf("log 初始化失败: %v", err))
 	}
+	return logger
+}
+
+func NewProdLoggger(filePath, level string) *zap.Logger {
+
+	hook := lumberjack.Logger{
+		Filename:   filePath, // 日志文件路径
+		MaxSize:    1,        // 每个日志文件保存的最大尺寸 单位：M
+		MaxBackups: 30,       // 日志文件最多保存多少个备份
+		MaxAge:     7,        // 文件最多保存多少天
+		Compress:   true,     // 是否压缩
+	}
+
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "linenum",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,  // 小写编码器
+		EncodeTime:     zapcore.ISO8601TimeEncoder,     // ISO8601 UTC 时间格式
+		EncodeDuration: zapcore.SecondsDurationEncoder, //
+		EncodeCaller:   zapcore.FullCallerEncoder,      // 全路径编码器
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+
+	// 设置日志级别
+	atomicLevel := zap.NewAtomicLevel()
+	if level == "info" {
+		atomicLevel.SetLevel(zap.InfoLevel)
+	} else if level == "debug" {
+		atomicLevel.SetLevel(zap.DebugLevel)
+	} else {
+		atomicLevel.SetLevel(zap.DebugLevel)
+	}
+	var writers zapcore.WriteSyncer
+
+	// if kafkaURL != "" {
+	// 	kp, err := NewKafkaProducer(constants.KafkaLogTopic, kafkaURL)
+	// 	if err == nil {
+	// 		writers = zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook), zapcore.AddSync(kp))
+	// 	}
+	// } else {
+	writers = zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook))
+	// }
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig), // 编码器配置
+		writers,                               // 打印到控制台和文件
+		atomicLevel,                           // 日志级别
+	)
+
+	// 开启开发模式，堆栈跟踪
+	caller := zap.AddCaller()
+	// 开启文件及行号
+	development := zap.Development()
+
+	logger := zap.New(core, caller, development, zap.AddStacktrace(zapcore.WarnLevel))
+
+	logger.Info("zap logger初始化成功")
+
 	return logger
 }
