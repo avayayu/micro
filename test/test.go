60e984ade679de49ef633a28f763bb62a2c7b0b8
zouhy <zouhangyu@buffalo-robot.com>
2020-11-20 14:22:15 +0800

    fix test add Content-Type

--- test/test.go
+++ test/test.go
@@ -45,7 +45,7 @@ func Get(t *testing.T, uri string, router *http.Engine) []byte {
 func PostForm(t *testing.T, uri string, param map[string]string, router *http.Engine) []byte {
 	// 构造post请求，表单数据以querystring的形式加在uri之后
 	req := httptest.NewRequest("POST", uri+ParseToStr(param), nil)
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
 	// 初始化响应
 	w := httptest.NewRecorder()
 
@@ -68,7 +68,7 @@ func PostJson(t *testing.T, uri string, param interface{}, router *http.Engine) 
 
 	// 构造post请求，json数据以请求body的形式传递
 	req := httptest.NewRequest("POST", uri, bytes.NewReader(jsonByte))
-
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 	// 初始化响应
 	w := httptest.NewRecorder()
 
