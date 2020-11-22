b4d6924a0a8e3de23f2f25488a1af1e86fab6057
zouhy <zouhangyu@buffalo-robot.com>
2020-11-22 13:31:18 +0800

    asda

--- test/test.go
+++ test/test.go
@@ -32,7 +32,7 @@ func ParseToStr(mp map[string]string) string {
 }
 
 // Get 根据特定请求uri，发起get请求返回响应
-func Get(t *testing.T, uri string, router *http.Engine, codeExpect int) (int, []byte) {
+func Get(t *testing.T, uri string, router *http.Engine) (int, []byte) {
 	// 构造get请求
 	req := httptest.NewRequest("GET", uri, nil)
 	// 初始化响应
@@ -51,7 +51,7 @@ func Get(t *testing.T, uri string, router *http.Engine, codeExpect int) (int, []
 }
 
 // PostForm 根据特定请求uri和参数param，以表单形式传递参数，发起post请求返回响应
-func PostForm(t *testing.T, uri string, param map[string]string, router *http.Engine, codeExpect int) (int, []byte) {
+func PostForm(t *testing.T, uri string, param map[string]string, router *http.Engine) (int, []byte) {
 	// 构造post请求，表单数据以querystring的形式加在uri之后
 	req := httptest.NewRequest("POST", uri+ParseToStr(param), nil)
 
@@ -71,7 +71,7 @@ func PostForm(t *testing.T, uri string, param map[string]string, router *http.En
 }
 
 // PostJson 根据特定请求uri和参数param，以Json形式传递参数，发起post请求返回响应
-func PostJson(t *testing.T, uri string, param interface{}, router *http.Engine, codeExpect int) (int, []byte) {
+func PostJson(t *testing.T, uri string, param interface{}, router *http.Engine) (int, []byte) {
 	// 将参数转化为json比特流
 	jsonByte, _ := json.Marshal(param)
 
