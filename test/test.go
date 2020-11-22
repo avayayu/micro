cbb12cfcb3ad5f506be616b6a5bf3f401024fb31
zouhy <zouhangyu@buffalo-robot.com>
2020-11-22 13:27:49 +0800

    add http Code to test.Add Http Result function

--- test/test.go
+++ test/test.go
@@ -11,9 +11,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type ResultType uint8 
+type ResultType uint8
 
-
+const (
+	Success ResultType = iota // 0
+	Fail                      // 1
+	JsonParseError
+	UnKnown
+)
 
 // ParseToStr 将map中的键值对输出成querystring形式
 func ParseToStr(mp map[string]string) string {
@@ -27,7 +32,7 @@ func ParseToStr(mp map[string]string) string {
 }
 
 // Get 根据特定请求uri，发起get请求返回响应
-func Get(t *testing.T, uri string, router *http.Engine, codeExpect int) []byte {
+func Get(t *testing.T, uri string, router *http.Engine, codeExpect int) (int, []byte) {
 	// 构造get请求
 	req := httptest.NewRequest("GET", uri, nil)
 	// 初始化响应
@@ -42,11 +47,11 @@ func Get(t *testing.T, uri string, router *http.Engine, codeExpect int) []byte {
 
 	// 读取响应body
 	body, _ := ioutil.ReadAll(result.Body)
-	return body
+	return w.Code, body
 }
 
 // PostForm 根据特定请求uri和参数param，以表单形式传递参数，发起post请求返回响应
-func PostForm(t *testing.T, uri string, param map[string]string, router *http.Engine, codeExpect int) []byte {
+func PostForm(t *testing.T, uri string, param map[string]string, router *http.Engine, codeExpect int) (int, []byte) {
 	// 构造post请求，表单数据以querystring的形式加在uri之后
 	req := httptest.NewRequest("POST", uri+ParseToStr(param), nil)
 
@@ -62,11 +67,11 @@ func PostForm(t *testing.T, uri string, param map[string]string, router *http.En
 
 	// 读取响应body
 	body, _ := ioutil.ReadAll(result.Body)
-	return body
+	return w.Code, body
 }
 
 // PostJson 根据特定请求uri和参数param，以Json形式传递参数，发起post请求返回响应
-func PostJson(t *testing.T, uri string, param interface{}, router *http.Engine, codeExpect int) []byte {
+func PostJson(t *testing.T, uri string, param interface{}, router *http.Engine, codeExpect int) (int, []byte) {
 	// 将参数转化为json比特流
 	jsonByte, _ := json.Marshal(param)
 
@@ -85,22 +90,25 @@ func PostJson(t *testing.T, uri string, param interface{}, router *http.Engine, 
 
 	// 读取响应body
 	body, _ := ioutil.ReadAll(result.Body)
-	return body
+	return w.Code, body
 }
 
-func IsFail(body []byte) bool {
+func Result(body []byte) ResultType {
 
 	result := map[string]interface{}{}
 	err := json.Unmarshal(body, &result)
 	if err != nil {
-		return false
+		return JsonParseError
 	}
 
-	if status,ok := result["status"];ok {
+	if status, ok := result["status"]; ok {
+		if status == "success" {
+			return Success
+		} else {
+			return Fail
+		}
 
+	} else {
+		return UnKnown
 	}
-}
-
-func IsSuccess(body []byte) bool {
-
 }
