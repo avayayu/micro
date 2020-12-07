8a86e1de1563f70f39a20ac34094a8d4d4e84c7b
zouhy <zouhangyu@buffalo-robot.com>
2020-12-07 13:57:27 +0800

    mobile phone verify

--- lib/utils.go
+++ lib/utils.go
@@ -232,6 +232,14 @@ func VerifyEmailFormat(email string) bool {
 	return reg.MatchString(email)
 }
 
+//VerifyMobileFormat 验证手机号格式是否正确
+func VerifyMobileFormat(mobileNum string) bool {
+	regular := "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$"
+
+	reg := regexp.MustCompile(regular)
+	return reg.MatchString(mobileNum)
+}
+
 //VeriyfiMongoErr 验证是否为空
 //为空返回true
 //不是空错误返回false
