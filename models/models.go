f8979461775d2423b350e8ba83a31b6dee34c1b2
zouhy <zouhangyu@buffalo-robot.com>
2020-12-10 16:12:36 +0800

    add redis primary tags to baseModel

--- models/models.go
+++ models/models.go
@@ -40,7 +40,7 @@ func init() {
 
 //Model 所有表格都有的基础模型
 type Model struct {
-	ID        uint64         `gorm:"column:id;type:bigint;not null;primaryKey;" json:"-"`                                     // 主键
+	ID        uint64         `gorm:"column:id;type:bigint;not null;primaryKey;" json:"-" redis:"primary"`                     // 主键
 	CreatedAt ztime.Time     `gorm:"column:created_at;type:datetime;not null;" json:"-" form:"created_at"`                    // 创建时间
 	UpdatedAt ztime.Time     `gorm:"column:updated_at;type:datetime; null;" json:"-" form:"updated_at"`                       // 更新时间
 	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at;type:datetime;index:delete_at_index;null;" json:"-" form:"deleted_at" ` //删除时间
